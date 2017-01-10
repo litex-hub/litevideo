6ec5780332415d1e8a8ffe46d67a120f1fb75b54
Florent Kermarrec <florent@enjoy-digital.fr>
2017-01-10 12:01:00 +0100

    csc/test: improve ycbcr422to444_tb

--- litevideo/csc/test/ycbcr422to444_tb.py
+++ litevideo/csc/test/ycbcr422to444_tb.py
@@ -1,3 +1,6 @@
+import random
+import unittest
+
 from litex.gen import *
 from litex.soc.interconnect.stream import *
 
@@ -5,32 +8,67 @@ from litevideo.csc.common import *
 from litevideo.csc.ycbcr422to444 import YCbCr422to444
 
 
-class TB(Module):
-    def __init__(self):
-        self.submodules.ycbcr422to444 = YCbCr422to444()
+prng = random.Random(42)
+reference_y = [prng.randrange(256) for i in range(32)]
+reference_cb = []
+reference_cr = []
+reference_cb_cr = [prng.randrange(256) for i in range(32)]
+for i in range(len(reference_cb_cr)//2):
+    cb = reference_cb_cr[2*i + 0]
+    reference_cb.append(cb)
+    reference_cb.append(cb)
+for i in range(len(reference_cb_cr)//2):
+    cr = reference_cb_cr[2*i + 1]
+    reference_cr.append(cr)
+    reference_cr.append(cr)
+
+sink_y = reference_y
+sink_cb_cr = reference_cb_cr 
+
+source_y = []
+source_cb = []
+source_cr = []
 
 
-def main_generator(dut):
-    yield dut.ycbcr422to444.source.ready.eq(1)
-    for i in range(16):
-        yield
-    for i in range(8):
-        yield dut.ycbcr422to444.sink.valid.eq(1)
-        yield dut.ycbcr422to444.datapath.first.eq(1)
-        yield dut.ycbcr422to444.sink.y.eq(0x1)
-        yield dut.ycbcr422to444.sink.cb_cr.eq(0x11)
-        yield
-        yield dut.ycbcr422to444.sink.valid.eq(1)
-        yield dut.ycbcr422to444.datapath.first.eq(0)
-        yield dut.ycbcr422to444.sink.y.eq(0x2)
-        yield dut.ycbcr422to444.sink.cb_cr.eq(0x12)
-        yield
-        yield dut.ycbcr422to444.sink.valid.eq(0)
-        for j in range(i):
+def sink_generator(sink, rand_threshold=100):
+    prng = random.Random(42)
+    for i in range(len(sink_cb_cr)):
+        valid = (prng.randrange(100) < rand_threshold)
+        if valid:
+            yield sink.valid.eq(1)
+            yield sink.y.eq(sink_y[i])
+            yield sink.cb_cr.eq(sink_cb_cr[i])
             yield
+            while not (yield sink.ready):
+               yield
+            yield sink.valid.eq(0)
+        else:
+            yield sink.valid.eq(0)
+            yield
+    # yield due to latency
+    for i in range(128):
+        yield
+
+@passive
+def source_generator(source, rand_threshold=100):
+    prng = random.Random(42)
+    while True:
+        ready = (prng.randrange(100) < rand_threshold)
+        yield source.ready.eq(ready)
+        if ready & (yield source.valid):
+            source_y.append((yield source.y))
+            source_cb.append((yield source.cb))
+            source_cr.append((yield source.cr))
+        yield
 
 if __name__ == "__main__":
-    tb = TB()
-    generators = {"sys" :   [main_generator(tb)]}
+    tb = YCbCr422to444()
+    generators = {"sys" :   [source_generator(tb.source),
+                             sink_generator(tb.sink)]}
     clocks = {"sys": 10}
     run_simulation(tb, generators, clocks, vcd_name="sim.vcd")
+
+    testcase = unittest.TestCase()
+    testcase.assertEqual(source_y, reference_y)
+    testcase.assertEqual(source_cb, reference_cb)
+    testcase.assertEqual(source_cr, reference_cr)
