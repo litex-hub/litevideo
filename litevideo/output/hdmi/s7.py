cefeda7d0f65782089f118c8297f481228c59089
Florent Kermarrec <florent@enjoy-digital.fr>
2017-04-05 15:02:23 +0200

    output/hdmi/s7: some cleanup

--- litevideo/output/hdmi/s7.py
+++ litevideo/output/hdmi/s7.py
@@ -20,96 +20,51 @@ class S7HDMIOutEncoderSerializer(Module):
 
         # # #
 
-        rst = ResetSignal("pix")
         ce = Signal()
-        self.sync.pix += ce.eq(~rst)
+        self.sync.pix += ce.eq(~ResetSignal("pix"))
 
         shift = Signal(2)
         pad_se = Signal()
 
         # OSERDESE2 master
-        self.specials += Instance("OSERDESE2",
-            p_DATA_RATE_OQ="DDR",
-            p_DATA_RATE_TQ="DDR",
-            p_DATA_WIDTH=10,
-            p_INIT_OQ=1,
-            p_INIT_TQ=1,
-            p_SERDES_MODE="MASTER",
-            p_SRVAL_OQ=0,
-            p_SRVAL_TQ=0,
-            p_TBYTE_CTL="FALSE",
-            p_TBYTE_SRC="FALSE",
-            p_TRISTATE_WIDTH=1,
+        self.specials += [
+            Instance("OSERDESE2",
+                p_DATA_WIDTH=10, p_TRISTATE_WIDTH=1,
+                p_DATA_RATE_OQ="DDR", p_DATA_RATE_TQ="DDR",
+                p_SERDES_MODE="MASTER",
 
-            o_OQ=pad_se,
-            i_CLK=ClockSignal("pix5x"),
-            i_CLKDIV=ClockSignal("pix"),
-            i_D1=self.data[0],
-            i_D2=self.data[1],
-            i_D3=self.data[2],
-            i_D4=self.data[3],
-            i_D5=self.data[4],
-            i_D6=self.data[5],
-            i_D7=self.data[6],
-            i_D8=self.data[7],
-            i_OCE=ce,
-            i_RST=rst,
+                o_OQ=pad_se,
+                i_OCE=ce,
+                i_TCE=0,
+                i_RST=ResetSignal("pix"),
+                i_CLK=ClockSignal("pix5x"), i_CLKDIV=ClockSignal("pix"),
+                i_D1=self.data[0], i_D2=self.data[1],
+                i_D3=self.data[2], i_D4=self.data[3],
+                i_D5=self.data[4], i_D6=self.data[5],
+                i_D7=self.data[6], i_D8=self.data[7],
 
-            i_SHIFTIN1=shift[0],
-            i_SHIFTIN2=shift[1],
-            i_T1=0,
-            i_T2=0,
-            i_T3=0,
-            i_T4=0,
-            i_TBYTEIN=0,
-            i_TCE=0
-        )
+                i_SHIFTIN1=shift[0], i_SHIFTIN2=shift[1],
+                #o_SHIFTOUT1=, o_SHIFTOUT2=,
+            ),
+            Instance("OSERDESE2",
+                p_DATA_WIDTH=10, p_TRISTATE_WIDTH=1,
+                p_DATA_RATE_OQ="DDR", p_DATA_RATE_TQ="DDR",
+                p_SERDES_MODE="SLAVE",
 
-        # OSERDESE2 slave
-        self.specials += Instance("OSERDESE2",
-            p_DATA_RATE_OQ="DDR",
-            p_DATA_RATE_TQ="DDR",
-            p_DATA_WIDTH=10,
-            p_INIT_OQ=1,
-            p_INIT_TQ=1,
-            p_SERDES_MODE="SLAVE",
-            p_SRVAL_OQ=0,
-            p_SRVAL_TQ=0,
-            p_TBYTE_CTL="FALSE",
-            p_TBYTE_SRC="FALSE",
-            p_TRISTATE_WIDTH=1,
+                i_OCE=ce,
+                i_TCE=0,
+                i_RST=ResetSignal("pix"),
+                i_CLK=ClockSignal("pix5x"), i_CLKDIV=ClockSignal("pix"),
+                i_D1=0, i_D2=0,
+                i_D3=self.data[8], i_D4=self.data[9],
+                i_D5=0, i_D6=0,
+                i_D7=0, i_D8=0,
 
-            i_CLK=ClockSignal("pix5x"),
-            i_CLKDIV=ClockSignal("pix"),
-
-            o_SHIFTOUT1=shift[0],
-            o_SHIFTOUT2=shift[1],
-
-            i_D1=0,
-            i_D2=0,
-            i_D3=self.data[8],
-            i_D4=self.data[9],
-            i_D5=0,
-            i_D6=0,
-            i_D7=0,
-            i_D8=0,
-            i_OCE=ce,
-            i_RST=rst,
-
-            i_SHIFTIN1=0,
-            i_SHIFTIN2=0,
-            i_T1=0,
-            i_T2=0,
-            i_T3=0,
-            i_T4=0,
-            i_TBYTEIN=0,
-            i_TCE=0
-        )
-
-        self.specials += Instance("OBUFDS",
-                p_IOSTANDARD="TDMS_33", p_SLEW="FAST",
-                i_I=pad_se, o_O=pad_p, o_OB=pad_n
-        )
+                i_SHIFTIN1=0, i_SHIFTIN2=0,
+                o_SHIFTOUT1=shift[0], o_SHIFTOUT2=shift[1]
+            ),
+            Instance("OBUFDS", i_I=pad_se, o_O=pad_p, o_OB=pad_n)
+        ]
 
 
 # This assumes a 100MHz base clock
@@ -135,26 +90,26 @@ class S7HDMIOutClocking(Module, AutoCSR):
         drp_do = Signal(16)
 
         self.specials += Instance("MMCME2_ADV",
-                    p_BANDWIDTH="OPTIMIZED", i_RST=0, o_LOCKED=mmcm_locked,
+            p_BANDWIDTH="OPTIMIZED", i_RST=0, o_LOCKED=mmcm_locked,
 
-                    # VCO
-                    p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=10.0,
-                    p_CLKFBOUT_MULT_F=30.0, p_CLKFBOUT_PHASE=0.000, p_DIVCLK_DIVIDE=4,
-                    i_CLKIN1=ClockSignal("clk100"), i_CLKFBIN=mmcm_fb, o_CLKFBOUT=mmcm_fb,
+            # VCO
+            p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=10.0,
+            p_CLKFBOUT_MULT_F=30.0, p_CLKFBOUT_PHASE=0.000, p_DIVCLK_DIVIDE=4,
+            i_CLKIN1=ClockSignal("clk100"), i_CLKFBIN=mmcm_fb, o_CLKFBOUT=mmcm_fb,
 
-                    # CLK0
-                    p_CLKOUT0_DIVIDE_F=5.0, p_CLKOUT0_PHASE=0.000, o_CLKOUT0=self.cd_pix.clk,
-                    # CLK1
-                    p_CLKOUT1_DIVIDE=1, p_CLKOUT1_PHASE=0.000, o_CLKOUT1=self.cd_pix5x.clk,
+            # CLK0
+            p_CLKOUT0_DIVIDE_F=5.0, p_CLKOUT0_PHASE=0.000, o_CLKOUT0=self.cd_pix.clk,
+            # CLK1
+            p_CLKOUT1_DIVIDE=1, p_CLKOUT1_PHASE=0.000, o_CLKOUT1=self.cd_pix5x.clk,
 
-                    # DRP
-                    i_DCLK=ClockSignal(),
-                    i_DWE=self.drp_dwe.storage,
-                    i_DEN=self.drp_den.re & self.drp_den.r,
-                    o_DRDY=drp_drdy,
-                    i_DADDR=self.drp_addr.storage,
-                    i_DI=self.drp_di.storage,
-                    o_DO=drp_do
+            # DRP
+            i_DCLK=ClockSignal(),
+            i_DWE=self.drp_dwe.storage,
+            i_DEN=self.drp_den.re & self.drp_den.r,
+            o_DRDY=drp_drdy,
+            i_DADDR=self.drp_addr.storage,
+            i_DI=self.drp_di.storage,
+            o_DO=drp_do
         )
         self.sync += [
             If(drp_drdy, self.drp_do.status.eq(drp_do)),
