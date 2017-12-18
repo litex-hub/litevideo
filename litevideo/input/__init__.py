c9770cc2da2df70c941376e9b937a2980822c739
Florent Kermarrec <florent@enjoy-digital.fr>
2017-12-18 13:34:10 +0100

    input/hdmi_in: make edid optional

--- litevideo/input/__init__.py
+++ litevideo/input/__init__.py
@@ -27,7 +27,8 @@ datacapture_cls = {
 class HDMIIn(Module, AutoCSR):
     def __init__(self, pads, dram_port=None, n_dma_slots=2, fifo_depth=512, device="xc6",
         default_edid=_default_edid, polarities=[0, 0, 0], clkin_freq=148.5e6):
-        self.submodules.edid = EDID(pads, default_edid)
+        if hasattr(pads, "scl"):
+            self.submodules.edid = EDID(pads, default_edid)
         self.submodules.clocking = clocking_cls[device](pads, clkin_freq)
 
         for datan in range(3):
