litex-hub/litevideo

commit 784cc8cbe91f5bebe18616bd60d484ac43bd0526

changes needed for a basic genlock
Shouldn't break backward compatibility.

However, I've just discovered that the hcounter/vcounter puts
active first, but HDMI transmits with sync first. This causes
an offset in the timing, so probably this code will get
refactored.

litevideo/input/clocking.py
@@ -101,16 +101,16 @@ class S7Clocking(Module, AutoCSR):
 
         self.locked = Signal()
         self.clock_domains.cd_pix = ClockDomain()
-        self.clock_domains.cd_pix_o = ClockDomain()
         self.clock_domains.cd_pix1p25x = ClockDomain()
         self.clock_domains.cd_pix5x = ClockDomain(reset_less=True)
-        self.clock_domains.cd_pix5x_o = ClockDomain(reset_less=True)
 
         if split_clocking:
             self._mmcm_write_o = CSR()
             self._mmcm_read_o = CSR()
             self._mmcm_dat_o_r = CSRStatus(16)
             self._mmcm_drdy_o = CSRStatus()
+            self.clock_domains.cd_pix_o = ClockDomain()
+            self.clock_domains.cd_pix5x_o = ClockDomain(reset_less=True)
 
         # # #
 
@@ -217,11 +217,6 @@ class S7Clocking(Module, AutoCSR):
                     self._mmcm_drdy_o.status.eq(1)
                 )
             ]
-        else:
-            self.comb += [
-                self.cd_pix_o.clk.eq(self.cd_pix.clk),
-                self.cd_pix5x_o.clk.eq(self.cd_pix5x.clk)
-            ]
 
         self.specials += MultiReg(mmcm_locked, self.locked, "sys")
         self.comb += self._locked.status.eq(self.locked)
@@ -233,6 +228,4 @@ class S7Clocking(Module, AutoCSR):
 
         if split_clocking:
             self.specials += AsyncResetSynchronizer(self.cd_pix_o, ~mmcm_locked_o)
-        else:
-            self.comb += self.cd_pix_o.rst.eq(self.cd_pix.rst)
 

litevideo/output/core.py
@@ -99,6 +99,8 @@ class TimingGenerator(Module):
 
         hcounter = Signal(hbits)
         vcounter = Signal(vbits)
+        self.hcounter = hcounter
+        self.vcounter = vcounter
 
         self.comb += [
             If(sink.valid,
@@ -154,7 +156,7 @@ class VideoOutCore(Module, AutoCSR):
 
     Generates a video stream from memory.
     """
-    def __init__(self, dram_port, mode="rgb", fifo_depth=512):
+    def __init__(self, dram_port, mode="rgb", fifo_depth=512, genlock=False, genlock_signal=None):
         try:
             dw = modes_dw[mode]
         except:
@@ -176,9 +178,13 @@ class VideoOutCore(Module, AutoCSR):
         self.submodules.dma = dma = ClockDomainsRenamer(cd)(DMAReader(dram_port, fifo_depth))
 
         # ctrl path
+        if genlock:
+            self.comb += timing.sink.valid.eq(initiator.source.valid & ~genlock_signal)
+        else:
+            self.comb += timing.sink.valid.eq(initiator.source.valid)
+
         self.comb += [
             # dispatch initiator parameters to timing & dma
-            timing.sink.valid.eq(initiator.source.valid),
             dma.sink.valid.eq(initiator.source.valid),
             initiator.source.ready.eq(timing.sink.ready),
 

commit 411669b7922a09e495023cb5856b5554c67c320d

input/clocking/S7Clocking: fix clock-domains when split_clocking=False
This reverts the regression introduced by commit 784cc8cbe. Previously
similar fix was done in commit 9b4169d5.

litevideo/input/clocking.py
@@ -101,16 +101,16 @@ class S7Clocking(Module, AutoCSR):
 
         self.locked = Signal()
         self.clock_domains.cd_pix = ClockDomain()
+        self.clock_domains.cd_pix_o = ClockDomain()
         self.clock_domains.cd_pix1p25x = ClockDomain()
         self.clock_domains.cd_pix5x = ClockDomain(reset_less=True)
+        self.clock_domains.cd_pix5x_o = ClockDomain(reset_less=True)
 
         if split_clocking:
             self._mmcm_write_o = CSR()
             self._mmcm_read_o = CSR()
             self._mmcm_dat_o_r = CSRStatus(16)
             self._mmcm_drdy_o = CSRStatus()
-            self.clock_domains.cd_pix_o = ClockDomain()
-            self.clock_domains.cd_pix5x_o = ClockDomain(reset_less=True)
 
         # # #
 
@@ -218,6 +218,11 @@ class S7Clocking(Module, AutoCSR):
                     self._mmcm_drdy_o.status.eq(1)
                 )
             ]
+        else:
+            self.comb += [
+                self.cd_pix_o.clk.eq(self.cd_pix.clk),
+                self.cd_pix5x_o.clk.eq(self.cd_pix5x.clk)
+            ]
 
         self.specials += MultiReg(mmcm_locked, self.locked, "sys")
         self.comb += self._locked.status.eq(self.locked)
@@ -229,4 +234,6 @@ class S7Clocking(Module, AutoCSR):
 
         if split_clocking:
             self.specials += AsyncResetSynchronizer(self.cd_pix_o, ~mmcm_locked_o)
+        else:
+            self.comb += self.cd_pix_o.rst.eq(self.cd_pix.rst)
 